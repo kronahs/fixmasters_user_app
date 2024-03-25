87705cc313a70db367800020c2c11dbeb2b8d253
unknown <naodtadele1@gmail.com>
2024-03-25 19:35:52 +0300

    finished loginpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:fixmasters_user_app/view/screens/auth_screen/loginPage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(const MyApp());
 }
 
@@ -15,11 +19,35 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
-        useMaterial3: true,
-        fontFamily: GoogleFonts.montserrat().fontFamily
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange, brightness: Brightness.light),
+        useMaterial3: false,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold
+          ),
+            // titleSmall: TextStyle(
+            //     fontWeight: FontWeight.bold
+            // )
+        ),
+      ),
+
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange, brightness: Brightness.dark),
+        useMaterial3: false,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+              fontWeight: FontWeight.bold
+          ),
+          // titleSmall: TextStyle(
+          //     fontWeight: FontWeight.bold
+          // )
+        ),
       ),
       home: LoginPage(),
+      themeMode: ThemeMode.dark,
     );
   }
 }
--- lib/view/screens/auth_screen/loginPage.dart
+++ lib/view/screens/auth_screen/loginPage.dart
@@ -1,16 +1,134 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Text('Login')
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 14.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 10),
+                Text('Login', style: Theme.of(context).textTheme.headline6,),
+                SizedBox(height: 6),
+                Text('Welcome back', style: Theme.of(context).textTheme.bodyText2,),
+
+                SizedBox(height: 30),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Email'),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter your email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    SizedBox(height: 20),
+
+                    Text('Password'),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter your password',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                      ),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Forgot password?'),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 20),
+
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Sign in'),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 14.0),
+                    minimumSize: Size(double.infinity, 0),
+                  ),
+                ),
+
+                SizedBox(height: 25),
+
+                Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        color: Theme.of(context).colorScheme.onBackground.withOpacity(0.2),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text('Or'),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Theme.of(context).colorScheme.onBackground.withOpacity(0.2),
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 25),
+
+                TextButton(
+                  onPressed: () {},
+                  child: Text('Sign in with Google'),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.onBackground.withOpacity(0.1),
+                    foregroundColor: Theme.of(context).colorScheme.onBackground,
+                    padding: EdgeInsets.symmetric(vertical: 14.0),
+                    minimumSize: Size(double.infinity, 0),
+                  ),
+                ),
+
+                Center(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Yet to register? ',
+                        style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Signup',
+                            style: TextStyle(color:Theme.of(context).colorScheme.primary), // Change color as needed
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
         ),
       ),
     );
